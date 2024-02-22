fd9b0211165dfebde695a444932f79ceea67469c
zhaobx2 <zhaobx2@sany.com.cn>
2024-02-22 15:44:54 +0800

    [feat] first commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+    printWidth: 100,
+    tabWidth: 2,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    arrowParens: 'always',
+    requirePragma: false,
+    insertPragma: false,
+    endOfLine: 'auto',
+    embeddedLanguageFormatting: 'auto',
+}
+  
